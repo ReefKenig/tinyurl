a7b2675f1ff253bf76ffa9d421f9617dc82a9c3c
Reef Kenig <rkenig@aol.com>
2023-08-06 17:22:39 +0300

    local postgres DB fully functional backend

--- tinyurl_backend/config.py
+++ tinyurl_backend/config.py
@@ -5,8 +5,8 @@ from functools import lru_cache
 class Settings(BaseSettings):
     env_name: str = "Local"
     base_url: str = "http://localhost:8000"
-    db_url: str = "sqlite:///./tinyurl.db"
-    # db_url: str = "postgresql+psychopg2://127.0.0.1:5432/tinyurl_db?user=postgres&password=RkPs090320"
+    # db_url: str = "sqlite:///./tinyurl.db"
+    db_url: str = "postgresql+psycopg2://127.0.0.1:5432/tinyurl_db?user=postgres&password=RkPs090320"
 
     class Config:
         env_file = ".env"
